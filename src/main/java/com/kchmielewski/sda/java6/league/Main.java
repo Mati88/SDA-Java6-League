dab9fbd8021c265131fe98823369402f6e370263
Krzysztof Chmielewski <chmielewski.k.s@gmail.com>
2018-01-21 20:11:51 +0100

    05: reading matches and teams from file

--- src/main/java/com/kchmielewski/sda/java6/league/Main.java
+++ src/main/java/com/kchmielewski/sda/java6/league/Main.java
@@ -1,6 +1,8 @@
 package com.kchmielewski.sda.java6.league;
 
 import com.kchmielewski.sda.java6.league.model.Leaderboard;
+import com.kchmielewski.sda.java6.league.model.Match;
+import com.kchmielewski.sda.java6.league.model.Team;
 import com.kchmielewski.sda.java6.league.reader.MatchReader;
 import com.kchmielewski.sda.java6.league.reader.StreamTeamReader;
 import com.kchmielewski.sda.java6.league.reader.TeamReader;
@@ -10,35 +12,92 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Objects;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Stream<Path> teams = Files.list(Paths.get("src/main/resources/teams"));
-        Stream<Path> matches = Files.list(Paths.get("src/main/resources/matches"));
+    public static void main(String[] args) {
+        ScheduledExecutorService service = Executors.newScheduledThreadPool(2);
         Leaderboard premierLeague = new Leaderboard();
+        String teams = "src/main/resources/teams";
+        String matches = "src/main/resources/matches";
+        String processed = "src/main/resources/processed";
+        String errors = "src/main/resources/error";
 
+        service.scheduleWithFixedDelay(teamReader(premierLeague, teams, processed, errors), 0, 1, TimeUnit.SECONDS);
+        service.scheduleWithFixedDelay(matchReader(premierLeague, matches, processed, errors), 0, 1, TimeUnit.SECONDS);
+        service.scheduleWithFixedDelay(() -> {
+            System.out.println("-------------------------------");
+            premierLeague.describe().forEach(System.out::println);
+            System.out.println("*******************************");
+        }, 0, 1, TimeUnit.SECONDS);
+    }
+
+    private static Runnable teamReader(Leaderboard leaderboard, String teams, String processed, String errors) {
         TeamReader teamReader = new StreamTeamReader();
-        teams.map(p -> {
+        return () -> {
             try {
-                return teamReader.read(p.toString());
+                Path initialPath = Paths.get(teams);
+                Stream<Path> teamPaths = Files.list(initialPath);
+                teamPaths.map(p -> {
+                    try {
+                        Team team = teamReader.read(p.toString());
+                        System.out.println(String.format("Successfully read team %s from file %s", p.getFileName().toString(), p));
+                        Files.move(p, Paths.get(processed + "/" + p.getFileName().toString()));
+                        return team;
+                    } catch (Exception e) {
+                        System.out.println("There was a problem reading team: " + p);
+                        e.printStackTrace();
+                        try {
+                            Files.move(p, Paths.get(errors + "/" + p.getFileName().toString()));
+                        } catch (IOException e1) {
+                            System.out.println(String.format("Could not move %s to %s.", p, initialPath));
+                            e1.printStackTrace();
+                        }
+                        return null;
+                    }
+                }).filter(Objects::nonNull).forEach(leaderboard::addTeam);
             } catch (IOException e) {
+                System.out.println(String.format("There was a problem reading team directory %s. ", teams));
                 e.printStackTrace();
-                return null;
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        }).filter(Objects::nonNull).forEach(premierLeague::addTeam);
+        };
+    }
 
+    private static Runnable matchReader(Leaderboard leaderboard, String matches, String processed, String errors) {
         MatchReader matchReader = new MatchReader();
-        matches.map(p -> {
+        return () -> {
             try {
-                return matchReader.read(p.toString(), premierLeague.getTeams());
+                Path initialPath = Paths.get(matches);
+                Stream<Path> teamPaths = Files.list(initialPath);
+                teamPaths.map(p -> {
+                    try {
+                        Match match = matchReader.read(p.toString(), leaderboard.getTeams());
+                        System.out.println(String.format("Successfully read match %s from file %s", p.getFileName().toString(), p));
+                        Files.move(p, Paths.get(processed + "/" + p.getFileName().toString()));
+                        return match;
+                    } catch (Exception e) {
+                        System.out.println("There was a problem reading team: " + p);
+                        e.printStackTrace();
+                        try {
+                            Files.move(p, Paths.get(errors + "/" + p.getFileName().toString()));
+                        } catch (IOException e1) {
+                            System.out.println(String.format("Could not move %s to %s.", p, initialPath));
+                            e1.printStackTrace();
+                        }
+                        return null;
+                    }
+                }).filter(Objects::nonNull).forEach(leaderboard::addMatch);
             } catch (IOException e) {
+                System.out.println(String.format("There was a problem reading matches directory %s. ", matches));
                 e.printStackTrace();
-                return null;
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        }).filter(Objects::nonNull).forEach(premierLeague::addMatch);
-
-        premierLeague.describe().forEach(System.out::println);
-        premierLeague.getGoals().forEach(System.out::println);
+        };
     }
 }
